47aaea7913077267fceb32c9b773afa35713e083
parkjunghoon <go9018@naver.com>
2020-10-15 20:07:04 +0900

    add : rxkotlin dependency / main sample

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,8 @@
+import io.reactivex.Observable
+
+fun main() {
+    Observable // 생산자 : 데이터를 생산하여 전달
+        .just("Hello!!!","RxJava~?") // 생성 연산자
+        .map { it.dropLast(1)} // 변환 소비자 : 데이터의 가공
+        .subscribe(::println) // 소비자 : 데이터를 받아서 처리 (println)
+}
