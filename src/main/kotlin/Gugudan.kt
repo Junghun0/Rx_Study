d3e2159312da9b0be78c56fc60dfcc25802f86d4
parkjunghoon <go9018@naver.com>
2020-12-09 15:52:52 +0900

    add : gugudan flatmap example

--- src/main/kotlin/Gugudan.kt
+++ src/main/kotlin/Gugudan.kt
@@ -1,5 +1,7 @@
 import io.reactivex.Observable
+import io.reactivex.Observable.just
 import io.reactivex.Observable.range
+import io.reactivex.functions.Function
 import java.util.*
 
 fun main() {
@@ -10,14 +12,33 @@ fun main() {
 
     //1. 일반적 for 문
     for (row in 1..9) {
-        println("$dan * $row = ${dan*row}")
+        println("$dan * $row = ${dan * row}")
     }
 
     println()
 
     //2. for 문의 Observable 변환
-    val source : Observable<Int> = range(1, 9)
+    val source: Observable<Int> = range(1, 9)
     source.subscribe { row ->
-        println("$dan * $row = ${dan*row}")
+        println("$dan * $row = ${dan * row}")
     }
+
+    println()
+
+    //3. 사용자 함수 정의하기
+    val gugudan: Function<Int, Observable<String>> = Function { num ->
+        range(1, 9).map { row ->
+            "$num * $row = ${num * row}"
+        }
+    }
+    val source2 = Observable.just(dan).flatMap(gugudan)
+    source2.subscribe(System.out::println)
+
+    //4. flatMap() 함수 활용
+    val source3: Observable<String> = Observable.just(dan)
+            .flatMap { num ->
+                range(1, 9).map { row ->
+                    "$num * $row = ${dan * row}"
+                }
+            }
 }
